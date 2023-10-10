46d51d96d2c7a4864f43f99ff9b633594073135b
SamarthSeshadri <145439345+SamarthSeshadri@users.noreply.github.com>
2023-10-10 15:34:20 +0530

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,1 +1,10 @@
-cshvi
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from google.colab import drive
+drive.mount('/content/drive')
+
